be62c5202589094713f2dc25521539ed21370175
Hakan Yücetürk <57252113+hakan8252@users.noreply.github.com>
2024-02-08 20:49:52 +0300

    example ticker

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ def main():
 
     # Create sidebar for inputs
     st.sidebar.header("Input Parameters")
-    ticker_input = st.sidebar.text_input("Enter a ticker", "", placeholder = "Example ticker APPL")
+    ticker_input = st.sidebar.text_input("Enter a ticker", "", placeholder = "Example ticker AAPL")
     selected_ticker = st.sidebar.selectbox("Or select a ticker", tickers)
 
     # Allow user to enter a ticker manually
